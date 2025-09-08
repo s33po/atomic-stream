3bdbf693c650acfd9ccf6d7ccbe601cc41cae32c
Seepo <seep0@pm.me>
2025-09-08 19:58:52 +0300

    Create 40-services.sh

---
+++ files/scripts/40-services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+systemctl enable dconf-update.service
+systemctl enable bootc-fetch-apply-updates.service
+systemctl enable bootc-fetch-apply-updates.timer
+systemctl enable firewalld
+systemctl enable fwupd.service
+systemctl enable rpm-ostree-countme.service
+systemctl enable podman.socket
+systemctl enable cockpit.socket
+systemctl enable docker.socket
+systemctl enable libvirtd.service
