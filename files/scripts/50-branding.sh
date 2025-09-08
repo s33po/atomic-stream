ca549abd9a2bbc36e5e545721badf3ed3fbdd2af
Seepo <seep0@pm.me>
2025-09-08 20:10:34 +0300

    Update 50-branding.sh

--- files/scripts/50-branding.sh
+++ files/scripts/50-branding.sh
@@ -2,12 +2,8 @@
 
 set -xeuo pipefail
 
-dnf remove -y \
-    console-login-helper-messages
-
 dnf install -y \
     plymouth-theme-spinner
-
 
 kver=$(cd /usr/lib/modules && echo * | awk '{print $1}')
 dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
