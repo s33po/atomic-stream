636a8bd01d8001774c5fbdd2648793e57b3d9975
Seepo <seep0@pm.me>
2025-09-08 20:30:34 +0300

    Create 30-flatpaks.sh

---
+++ files/scripts/30-flatpaks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+tee -a /etc/flatpak/defpaks.list <<EOF
+org.mozilla.firefox
+org.gnome.Calendar
+org.gnome.NautilusPreviewer
+com.mattjakeman.ExtensionManager
+page.tesk.Refine
+org.gtk.Gtk3theme.adw-gtk3
+org.gtk.Gtk3theme.adw-gtk3-dark
+com.github.tchx84.Flatseal
+io.github.flattool.Warehouse
+io.github.dvlv.boxbuddyrs
+org.gnome.Boxes
+org.gnome.World.PikaBackup
+com.github.neithern.g4music
+io.mpv.Mpv
+org.libreoffice.LibreOffice
+it.mijorus.gearlever
+be.alexandervanhee.gradia
+EOF
