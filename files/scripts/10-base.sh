b82e40eea0828f8c7d2ead80d12f45393cbd1fe9
Seepo <seep0@pm.me>
2025-09-08 18:50:42 +0300

    add hyperscale kernel

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -4,8 +4,16 @@ set -xeuo pipefail
 
 dnf remove -y subscription-manager
 
-dnf install -y 'dnf-command(config-manager)' epel-release
-dnf config-manager --set-enabled crb
-dnf upgrade -y epel-release
+# EXPERIMENTAL: install hyperscale kernel (From TunaOS)
+dnf -y install centos-release-hyperscale-kernel
+dnf config-manager --set-disabled "centos-hyperscale,centos-hyperscale-kernel"
+dnf --enablerepo="centos-hyperscale" --enablerepo="centos-hyperscale-kernel" -y update kernel
 
-dnf install -y system-reinstall-bootc powertop fuse steam-devices
+dnf -y install 'dnf-command(versionlock)'
+dnf versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
+
+dnf -y install 'dnf-command(config-manager)' epel-release
+dnf config-manager --set-enabled crb
+dnf -y upgrade epel-release
+
+dnf -y install system-reinstall-bootc powertop fuse steam-devices
