96ebcc02646c5ef173ace7d999467d81649470a8
Seepo <seep0@pm.me>
2025-09-08 20:47:07 +0300

    feat: add defpaks and some other aliases

--- files/scripts/26-preferences.sh
+++ files/scripts/26-preferences.sh
@@ -6,4 +6,11 @@ set -xeuo pipefail
 cat << 'EOF' > /etc/profile.d/bash_aliases.sh
 alias ls='ls --color=auto'
 alias ll='ls -la'
+alias ..='cd ..'                 
+alias ...='cd ../..'             
+alias grep='grep --color=auto'  
+alias myip='curl ifconfig.me'              
+alias fixperms='sudo chown -R $USER:$USER'
+alias please='sudo $(fc -ln -1)'
+alias defpaks='xargs -a /etc/flatpaks/defpaks.list -r flatpak install -y --noninteractive flathub && echo "Flatpak installation complete!"'
 EOF
