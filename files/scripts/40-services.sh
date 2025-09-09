3aebb3ae1a3a91799862769fad44a08d8617e4fa
Seepo <seep0@pm.me>
2025-09-09 19:34:10 +0300

    cleanup: remove unnecessary services

--- files/scripts/40-services.sh
+++ files/scripts/40-services.sh
@@ -2,11 +2,8 @@
 
 set -xeuo pipefail
 
-systemctl enable bootc-fetch-apply-updates.service
 systemctl enable bootc-fetch-apply-updates.timer
 systemctl enable firewalld
-systemctl enable fwupd.service
-systemctl enable rpm-ostree-countme.service
 systemctl enable podman.socket
 systemctl enable cockpit.socket
 systemctl enable docker.socket
