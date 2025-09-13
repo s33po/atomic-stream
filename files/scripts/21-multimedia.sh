c82757b32ae51b37b57623d8fd43f59688017be5
Seepo <seep0@pm.me>
2025-09-13 19:15:59 +0300

    test: move multimedia before gnome  desktop installation

--- files/scripts/21-multimedia.sh
+++ files/scripts/21-multimedia.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 # Multimedia codecs from Negativo17 (from Bluefin LTS)
-dnf config-manager --add-repo=https://negativo17.org/repos/epel-multimedia.repo
-dnf config-manager --set-disabled epel-multimedia
-dnf -y install --enablerepo=epel-multimedia \
-	ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
+#dnf config-manager --add-repo=https://negativo17.org/repos/epel-multimedia.repo
+#dnf config-manager --set-disabled epel-multimedia
+#dnf -y install --enablerepo=epel-multimedia \
+#	ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
